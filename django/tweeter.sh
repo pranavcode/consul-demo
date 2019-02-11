196e47e36f8585437e25e801934d70d64ae6d27a
Pranav Kulkarni <pranavcode@gmail.com>
2019-02-11 23:07:00 +0530

    Adding gunicorn and djongo to django project

---
+++ django/tweeter.sh
@@ -0,0 +1,8 @@
+# Migrate DB for Django API
+python ./manage.py migrate
+
+# Fetch static files
+./manage.py collectstatic --noinput
+
+# Run background task processor and Django API server
+gunicorn -w 3 --bind 0.0.0.0:8000 --access-logfile - tweeter.wsgi:application
