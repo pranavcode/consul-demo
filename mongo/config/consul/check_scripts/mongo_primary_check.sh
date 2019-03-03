585c40b71f09d66c0a8d351caad2b98da1c0b744
Pranav Kulkarni <pranavcode@gmail.com>
2019-03-03 12:38:21 +0530

    Updating the docker compose to allow host network and improve exit code to standard warning

--- mongo/config/consul/check_scripts/mongo_primary_check.sh
+++ mongo/config/consul/check_scripts/mongo_primary_check.sh
@@ -2,5 +2,5 @@
 
 mongo_primary=$(mongo --quiet --eval 'JSON.stringify(db.isMaster())' | jq -r .ismaster 2> /dev/null)
 if [[ $mongo_primary == false ]]; then
-    exit 123
+    exit 1
 fi
--- mongo/config/consul/check_scripts/mongo_secondary_check.sh
+++ mongo/config/consul/check_scripts/mongo_secondary_check.sh
@@ -2,5 +2,5 @@
 
 mongo_secondary=$(mongo --quiet --eval 'JSON.stringify(db.isMaster())' | jq -r .secondary 2> /dev/null)
 if [[ $mongo_secondary == false ]]; then
-    exit 123
+    exit 1
 fi
